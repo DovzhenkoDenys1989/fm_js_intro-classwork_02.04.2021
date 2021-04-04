60378c443be966c5c988fc319fd2e15f52698369
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-04 21:34:00 +0300

    Add forEach example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const users = [{},{},{},{},{}];
+const myArrUsers = new MyArray({},{},{},{},{});
+
+const subscribe = function(elem){
+  elem.isSubscribed = true;
+}
+
+users.forEach(subscribe);
+
+myArrUsers.forEach(function(elem){
+  elem.isSubscribed = true;
+});
